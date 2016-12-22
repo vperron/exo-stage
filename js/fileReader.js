d5eedc4532b045392fc19f84b3ebf48fa3ddcc5f
teddy-toussaint <toussaintteddy@gmail.com>
2016-12-22 18:56:06 -0400

    Update

--- js/fileReader.js
+++ js/fileReader.js
@@ -1,4 +1,4 @@
-// Parses a CSV content into a JavaScript 2D array
+// Parses a CSV content into a sorted JavaScript 2D array
 function csvToJavaScript(txt) {
     
     // Regular expression definition
@@ -11,25 +11,62 @@ function csvToJavaScript(txt) {
     
     for (var i = 1; i < rentals.length; i++) {
         rental = rentals[i].split(",");
+        
         // JSON-like representation of each rental
         javaTable[i - 1] = {beg: rental[0],
                             end: rental[1],
                             type: rental[2],
                             dist: rental[3],
                             car: rental[4]};
+        
     }
     
-    return javaTable;
+    /*
+    // Sorts the rentals in ascending start time order
+    return javaTable.sort(function (a,b){
+        return new Date(a.beg) - new Date(b.beg);
+    });
+    */
+    
+    // Sorts the rentals in ascending start time order
+    javaTable = javaTable.sort(function (a,b){
+        return new Date(a.beg) - new Date(b.beg);
+    });
+    
+    for (var i in ("beg", "end")) {
+        
+    }
     
 }
 
 // Parses a JavaScript 2D array into an HTML table
-function javaScriptToHTML(table) {
+function javaScriptToHTML(javaTable) {
+    var tableElt = document.createElement("table");
+    tableElt.id = "table";
     
-}
-
-// Sorts an array of dates
-function sortByDate(dateArray) {
+    // For each rental
+    for (var i = 0; i < javaTable.length; i++) {
+        
+        var rentElts = new Array(javaTable.length);
+        rentElts[i] = document.createElement("tr");
+        
+        // For each data
+        for (var j in javaTable[i]) {
+            
+            var dataElts = new Array(5);
+            dataElts[j] = document.createElement("td");
+            dataElts[j].class = j;
+            dataElts[j].textContent = javaTable[i][j];
+            
+            rentElts[i].appendChild(dataElts[j]);
+            
+        }
+        
+        tableElt.appendChild(rentElts[i]);
+        
+    }
+    
+    return tableElt;
     
 }
 
@@ -46,15 +83,21 @@ if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
         // Checks if the file's extension
         if (!/\.csv$/i.test(fileListElt.files[0].name)) {
             // It is not a CSV
-            alert("Le fichier à fournir doit être de type CSV. Merci de réessayer.");
+            alert("Le fichier fourni doit être de type CSV. Merci de réessayer.");
         } else {
             
             // It is a CSV
             var reader = new FileReader();
             reader.addEventListener('load', function () {
+                
                 // From file's content to JavaScript table, to HTML table
-                //var htmlTable = javaScriptToHTML(csvToJavaScript(reader.result));
                 var javaTable = csvToJavaScript(reader.result);
+                var htmlTable = javaScriptToHTML(javaTable);
+                
+                document.body.appendChild(htmlTable);
+                
+                //TESTS
+                
             });
             // Reads the given file
             reader.readAsText(fileListElt.files[0]);

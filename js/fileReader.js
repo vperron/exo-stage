08725f87dd1acf34ef49e166f6b26ee3bb0bd349
teddy-toussaint <toussaintteddy@gmail.com>
2016-12-22 23:27:45 -0400

    Update

--- js/fileReader.js
+++ js/fileReader.js
@@ -15,30 +15,46 @@ function csvToJavaScript(txt) {
         // JSON-like representation of each rental
         javaTable[i - 1] = {beg: rental[0],
                             end: rental[1],
+                            // Duration of the rental
                             dura: (new Date(rental[1]) - new Date(rental[0])),
                             type: rental[2],
                             dist: rental[3],
                             car: rental[4]};
         
+        // Translation of variables 'beg', 'end' and 'dura' to an understandable format
+        //**************************************************************************************************************************
+        for (var j in javaTable[i - 1]) {
+            if ((j === "beg") || (j === "end")) {
+                var date = new Date(javaTable[i - 1][j]);
+                console.log(javaTable[i - 1][j]);
+                javaTable[i - 1][j] = date.getDate() + "/" +
+                                     (date.getMonth() + 1).toString() + "/" +
+                                     date.getFullYear() + ", " +
+                                     date.getHours() + ":" +
+                                     date.getMinutes() + ":" +
+                                     date.getSeconds();
+            } else if (j === "dura") {
+                
+            }
+            
+        }
+    
     }
     
-    
+    /*
     // Sorts the rentals in ascending start time order
     return javaTable.sort(function (a,b){
         return new Date(a.beg) - new Date(b.beg);
     });
+    */
     
     
-    /*
     // Sorts the rentals in ascending start time order
     javaTable = javaTable.sort(function (a,b){
         return new Date(a.beg) - new Date(b.beg);
     });
     
-    for (var i in ("beg", "end")) {
-        
-    }
-    */
+    return javaTable;    
     
 }
 
@@ -46,6 +62,27 @@ function csvToJavaScript(txt) {
 function javaScriptToHTML(javaTable) {
     var tableElt = document.createElement("table");
     tableElt.id = "table";
+    
+    // Head of the table
+    var tableHeadElt = document.createElement("tr");
+    tableHeadElt.id = "tableHead";
+    
+    var tableHeadDataElts = new Array(6);
+    for (var i = 0; i < 6; i++) {
+        tableHeadDataElts[i] = document.createElement("td");
+    }
+    tableHeadDataElts[0].textContent = "Début";
+    tableHeadDataElts[1].textContent = "Fin";
+    tableHeadDataElts[2].textContent = "Durée";
+    tableHeadDataElts[3].textContent = "Type";
+    tableHeadDataElts[4].textContent = "Distance";
+    tableHeadDataElts[5].textContent = "Voiture";
+    
+    for (var i = 0; i < 6; i++) {
+        tableHeadElt.appendChild(tableHeadDataElts[i]);
+    }
+    
+    tableElt.appendChild(tableHeadElt);
     
     // For each rental
     for (var i = 0; i < javaTable.length; i++) {
@@ -59,7 +96,7 @@ function javaScriptToHTML(javaTable) {
             var dataElts = new Array(6);
             dataElts[j] = document.createElement("td");
             dataElts[j].class = j;
-            dataElts[j].textContent = javaTable[i][j];
+            dataElts[j].textContent = javaTable[i].j;
             
             rentElts[i].appendChild(dataElts[j]);
             
@@ -89,6 +126,11 @@ if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
             // It is not a CSV
             alert("Le fichier fourni doit être de type CSV. Merci de réessayer.");
         } else {
+            
+            // Remove previous table
+            try {
+                document.body.removeChild(document.getElementById("table"));
+            } catch(e) {}
             
             // It is a CSV
             var reader = new FileReader();
